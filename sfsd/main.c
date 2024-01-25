2de6de679d49641e65da04f6d13a88e0e0ebae28
issamhxr2 <148063427+Issamhxr@users.noreply.github.com>
2024-01-25 02:56:29 +0100

    declarationjdida
    ...corect

--- sfsd/main.c
+++ sfsd/main.c
@@ -1,3 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+typedef struct {
+    int nbBlocs;
+    int nbfir;
+    int nblas;
+} entete;
+
+typedef struct Record {
+    char info[20];
+    int next;
+    int nb;
+
+    struct Record* svt;
+} Record;
+
+typedef struct {
+    int cle;
+    char info[200];
+   int deletecle
+
+} etudiant
+
+typedef struct {
+    Record* r;
+    entetf t;
+    FILE* physicalFile;
+    }Lnovnc;
+
 
 void Recuperer_chaine(int n, int i, int j, int *longeur, char *chaine) {
     *longeur = n;
